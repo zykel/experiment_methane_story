1439dd2093f2587ae1b46085501172cbc064ef6e
benedikt <benedikt.mayer@st.ovgu.de>
2024-11-03 23:06:37 +0100

    title etc.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  base: '/experiment_insta_progression/',
+  base: '/experiment_methane_story/',
   plugins: [svelte(), envCompatible()],
 });
