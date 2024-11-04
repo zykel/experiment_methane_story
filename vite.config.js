037af1bd9bcb2ca6d8323323e65a963d09afc5ca
benedikt <benedikt.mayer@st.ovgu.de>
2024-11-04 09:45:45 +0100

    Reverted deploy params

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  base: '/experiment_methane_story/',
   plugins: [svelte(), envCompatible()],
 });
